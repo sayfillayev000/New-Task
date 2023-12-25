42dbe0d7b7a4803d044926ed029d8f7cacee3d55
Muhammad <muhammadsayfillayev1027@gmail.com>
2023-12-26 00:02:52 +0500

    tugadi

--- src/pages/Orders/index.jsx
+++ src/pages/Orders/index.jsx
@@ -5,8 +5,8 @@ import { futbolka2 } from "../../assets/images";
 import { LeftIcon } from "../../assets/icons";
 
 const index = () => {
-  const data = sessionStorage.getItem("orders")
-  ? (sessionStorage.getItem("orders"))
+  const orderdata = sessionStorage.getItem("orders")
+  ? JSON.parse(sessionStorage.getItem("orders"))
   : [];
   const [id, setId] = useState();
   if (id) {
@@ -18,8 +18,8 @@ const index = () => {
   }
   return (
     <div className="container">
-      <h2 className="order__title">Orders ({data.length})</h2>
-      {data?.map((item) => (
+      <h2 className="order__title">Orders ({orderdata.length})</h2>
+      {orderdata && orderdata?.map((item) => (
         <div key={item.id} className="orders__container">
           <div className="order__card">
             <img className="order__left__img" src={item.img} alt="" />
